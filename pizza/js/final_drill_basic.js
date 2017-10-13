98a853043db29c2ae801adc1fb5ea1c7b3efaa7e
CorbinLBecker <32005531+CorbinLBecker@users.noreply.github.com>
2017-10-12 20:01:20 -0700

    Add files via upload

---
+++ pizza/js/final_drill_basic.js
@@ -0,0 +1,179 @@
+// Get the pizza size price and add it to the running Total
+// then pass that running total to the next function so 
+// the next function will add a particular total to the running total and so on...
+//
+// Keep doing this until you get all items added to the running total.
+//
+// Just remember that the syntax will be text1 = text1 + something + "<br>";
+// So you take the orginal value and concetenate to something new and end it with
+// an HTML line break so our code will write the next thing one line below instead
+// of overwriting the previous print out.
+function getReceipt() {
+	// This initializes our string so it can get passed from  
+	// function to function, growing line by line into a full receipt
+	var text1 = "<h3>You Ordered:</h3>";
+	var runningTotal = 0;
+	var sizeTotal = 0;
+	var sizeArray = document.getElementsByClassName("size");
+	for (var i = 0; i < sizeArray.length; i++) {
+		if (sizeArray[i].checked) {
+			var selectedSize = sizeArray[i].value;
+			text1 = text1+selectedSize+"<br>";
+		}
+	}
+	if (selectedSize === "Personal Pizza") {
+		sizeTotal = 6;
+	} else if (selectedSize === "Medium Pizza") {
+		sizeTotal = 10;
+	} else if (selectedSize === "Large Pizza") {
+		sizeTotal = 14;
+	} else if (selectedSize === "Extra Large Pizza") {
+		sizeTotal = 16;
+	}
+	runningTotal = sizeTotal;
+	getMeat(runningTotal,text1); // All three of these variables will be passed on to each function
+};	
+
+// With both the meat and veggie functions each item in the array will be
+// 1 dollar but the first is going to be free so we can count the total
+// of items in their array and subtract 1 to get the total item cost
+//
+// Now we can add the item cost to our running total to get the new
+// running total and then pass this new running total to the next function
+// Just keep up this process until we've added all items to the running total
+function getMeat(runningTotal,text1) {
+	var meatTotal = 0;
+	var selectedMeat = [];
+	var meatArray = document.getElementsByClassName("meats");
+	for (var j = 0; j < meatArray.length; j++) {
+		if (meatArray[j].checked) {
+			selectedMeat.push(meatArray[j].value);
+			text1 = text1+meatArray[j].value+"<br>";
+		}
+	}
+	var meatCount = selectedMeat.length;
+	if (meatCount > 1) {
+		meatTotal = (meatCount - 1);
+	} else {
+		meatTotal = 0;
+	}
+	runningTotal = (runningTotal + meatTotal);
+	getcheese(runningTotal,text1);
+};
+// cheeseee--
+
+function getcheese(runningTotal,text1) {
+	var cheeseTotal = 0;
+	var selectedCheese = [];
+	var cheeseArray = document.getElementsByClassName("cheese");
+	for (var j = 0; j < cheeseArray.length; j++) {
+		if (cheeseArray[j].checked) {
+			selectedCheese = cheeseArray[j].value;
+		}
+		if (selectedCheese === "Extra cheese") {
+			cheeseTotal = 3;
+			text1 = text1+cheeseArray[j].value+"<br>";
+		}
+	}
+	runningTotal = (runningTotal + cheeseTotal);
+	getSauce(runningTotal,text1);
+};
+// /cheese.  ---
+
+
+
+
+// saucey ---
+
+function getSauce(runningTotal,text1) {
+	var sauceTotal = 0;
+	var selectedSauce = [];
+	var sauceArray = document.getElementsByClassName("sauce");
+	for (var j = 0; j < sauceArray.length; j++) {
+		if (sauceArray[j].checked) {
+			selectedSauce= sauceArray[j].value;
+			text1 = text1+sauceArray[j].value+"<br>";
+		}
+	}
+		
+	runningTotal = (runningTotal + sauceTotal);
+	getveggies(runningTotal,text1);
+};
+
+// saucey ----
+
+
+
+
+
+// vegggieeeessss----
+
+function getveggies(runningTotal,text1) {
+	var veggiesTotal = 0;
+	var selectedveggies = [];
+	var veggiesArray = document.getElementsByClassName("veggies");
+	for (var j = 0; j < veggiesArray.length; j++) {
+		if (veggiesArray[j].checked) {
+			selectedveggies.push(veggiesArray[j].value);
+			console.log("selected veggies item: ("+veggiesArray[j].value+")");
+			text1 = text1+veggiesArray[j].value+"<br>";
+		}
+	}
+	var veggiesCount = selectedveggies.length;
+	if (veggiesCount > 1) {
+		veggiesTotal = (veggiesCount - 1);
+	} else {
+		veggiesTotal = 0;
+	}
+	runningTotal = (runningTotal + veggiesTotal);
+	getCrust(runningTotal,text1);
+};
+
+
+// vegggieeeessss----
+
+
+
+
+// crustpunk
+
+
+function getCrust(runningTotal,text1,) {
+	var crustTotal = 0;
+	var selectedCrust;
+	var crustArray = document.getElementsByClassName("crust");
+	for (var j = 0; j < crustArray.length; j++) {
+		if (crustArray[j].checked) {
+			selectedCrust = crustArray[j].value;
+			text1 = text1 + selectedCrust + "<br>";
+		}
+		if (selectedCrust === "Cheese Stuffed Crust") {
+			crustTotal = 3;
+		}
+	}
+	runningTotal = (runningTotal + crustTotal);
+	document.getElementById("showText").innerHTML=text1;
+	document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";
+};
+
+// crust
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
